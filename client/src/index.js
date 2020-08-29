6d7a41b25e37a0c496da11e01692011d3aa0ecd3
Tanish Agarwal <tanishagarwal1998@gmail.com>
2020-08-29 17:45:42 +0530

    change backend URI

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import './index.scss';
 import App from './App';
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000',
+	uri: 'http://192.168.0.105:4000',
 	cache: new InMemoryCache(),
 });
 
